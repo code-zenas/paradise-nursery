8471c972309bc9303a16de9c0830643438b5d48f
code-zenas <geenesh01@gmail.com>
2026-05-13 12:24:39 -0500

    Update App.jsx

--- e-plantShopping/src/App.jsx
+++ e-plantShopping/src/App.jsx
@@ -1,1 +1,22 @@
+import React from "react";
+import "./App.css";
 
+function App() {
+  const handleClick = () => {
+    alert("Get Started");
+  };
+
+  return (
+    <div className="background-image">
+      <div className="landing-page">
+        <h1>Welcome to Paradise Nursery</h1>
+
+        <button className="get-started-btn" onClick={handleClick}>
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
