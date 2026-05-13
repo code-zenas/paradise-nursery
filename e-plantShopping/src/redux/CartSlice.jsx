0024bae1c7acaac3068f39355cfcd35419b4e18d
code-zenas <geenesh01@gmail.com>
2026-05-13 12:25:02 -0500

    Update CartSlice.jsx

--- e-plantShopping/src/redux/CartSlice.jsx
+++ e-plantShopping/src/redux/CartSlice.jsx
@@ -1,1 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  items: [],
+};
+
+const CartSlice = createSlice({
+  name: "cart",
+  initialState,
+
+  reducers: {
+    addItem: (state, action) => {
+      state.items.push({
+        ...action.payload,
+        quantity: 1,
+      });
+    },
+
+    removeItem: (state, action) => {
+      state.items = state.items.filter(
+        (item) => item.id !== action.payload
+      );
+    },
+
+    updateQuantity: (state, action) => {
+      const item = state.items.find(
+        (item) => item.id === action.payload.id
+      );
+
+      if (item) {
+        item.quantity = action.payload.quantity;
+      }
+    },
+  },
+});
+
+export const {
+  addItem,
+  removeItem,
+  updateQuantity,
+} = CartSlice.actions;
+
+export default CartSlice.reducer;
