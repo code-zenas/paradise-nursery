62c8a01f000d8b0575270f5a768f40afd397f8af
code-zenas <geenesh01@gmail.com>
2026-05-13 12:33:32 -0500

    Update App.jsx

--- e-plantShopping/src/App.jsx
+++ e-plantShopping/src/App.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import ProductList from "./components/ProductList";
 
 function App() {
-  const handleClick = () => {
-    alert("Get Started");
+  const [showProductList, setShowProductList] =
+    useState(false);
+
+  const handleGetStarted = () => {
+    setShowProductList(true);
   };
 
   return (
-    <div className="background-image">
-      <div className="landing-page">
-        <h1>Welcome to Paradise Nursery</h1>
+    <div>
+      {showProductList ? (
+        <ProductList />
+      ) : (
+        <div className="background-image">
+          <div className="landing-page">
+            <h1>Welcome to Paradise Nursery</h1>
 
-        <button className="get-started-btn" onClick={handleClick}>
-          Get Started
-        </button>
-      </div>
+            <button
+              className="get-started-btn"
+              onClick={handleGetStarted}
+            >
+              Get Started
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
